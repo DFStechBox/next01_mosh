97fbeb44b70db1a44bc623024edbc7c252d3d86b
DFSTechBox <dfosterstewart@gmail.com>
2024-07-22 15:54:14 -0700

    Restarted the Next.js course

--- app/blog/[postid]/comments/[commentid]/page.tsx
+++ app/blog/[postid]/comments/[commentid]/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const page = ({params}) => {
+interface Props {
+  params: { id: number };
+  commentid: string;
+}
+
+const page = ({params}: Props) => {
   return (
     <div>
       <h1>Comment: {params.commentid} for blog post { params.postid}</h1>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,22 +1,19 @@
-
-import UserTable from './UserTable'
-
-
+import UserTable from "./UserTable";
+interface User {
+  id: number;
+  name: string;
+}
 
 const UsersPage = async () => {
-
-    
-
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
-      {/* <p>{new Date().toLocaleTimeString()}</p> */}
-      <hr />
-      <UserTable />
-      
+      <ul>{users.map(user => <li key={user.id}>{ user.name}</li>)}</ul>
+      {/* <UserTable /> */}
     </>
-    
-  )
-}
+  );
+};
 
-export default UsersPage
+export default UsersPage;
